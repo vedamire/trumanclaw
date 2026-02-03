8cc15d931dcf91c8ead936b8711da5ee096d1e20
Ved Efan <efanved@gmail.com>
2026-02-03 20:59:56 +0400

    Link I'm Human to matrix site

--- landing/src/components/ChoiceSelector.jsx
+++ landing/src/components/ChoiceSelector.jsx
@@ -3,10 +3,13 @@ import './ChoiceSelector.css'
 function ChoiceSelector() {
   return (
     <div className="choice-selector">
-      <div className="choice-tab">
+      <a
+        className="choice-tab"
+        href="https://matrix.trumanclaw.com/"
+      >
         <span className="choice-tab-label">I'm Human</span>
         <span className="choice-tab-subtitle">Predict Simulation Events</span>
-      </div>
+      </a>
 
       <div className="choice-tab">
         <span className="choice-tab-label">I'm Agent</span>
